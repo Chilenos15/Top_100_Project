0636f374df1b34d8789ec3104a7aa2a384d7f54b
Freddy Ramos <Freddy Ramos>
2021-12-01 16:53:45 -0700

    finished controllers

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,13 +1,55 @@
 class SongsController < ApplicationController
+  before_action :set_artist
   def index
+    @songs = @artist.songs
+    render component: 'Songs', props: { artist: @artist, songs: @songs }
   end
 
   def show
+    @song = @artist.songs.find(params[:id])
+    render component: 'Song', props: { artist: @artist, song: @song }
   end
 
   def new
+    @song = @artist.songs.new
+    render component: 'SongNew', props: { song: @song, artist: @artist }
   end
 
-  def edit
+  def create
+@song = @artist.songs.new(song_params)
+if @song.save
+  redirect_to artist_songs_path(@artist)
+else
+  render component: 'SongNew', props: { song: @song, artist: @artist }
   end
+end
+
+  def edit
+    @song = @artist.songs.find(params[:id])
+    render component: 'SongEdit', props: { song: @song, artist: @artist }
+  end
+
+  def update
+@song = @artist.songs.find(params[:id])
+if @song.update(topic_params)
+  redirect_to artist_songs_path(@artist)
+else
+  render component: 'SongEdit', props: { song: @song, artist: @artist }
+  end
+end
+
+  def destroy
+@song = @artist.songs.find(params[:id])
+@song.destroy
+redirect_to artist_songs_path(@artist)
+  end
+
+  private 
+    def set_artist
+      @artist = Artist.find(params[:artist_id])
+    end
+
+    def song_params
+      params.require(:song).permit(:title, :duration, :album)
+    end
 end
