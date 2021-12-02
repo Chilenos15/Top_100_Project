6bf46a1d6cdf1357c00d556a1035592a7d710223
Freddy Ramos <Freddy Ramos>
2021-12-02 16:46:08 -0700

    finished project

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,59 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # route is going to be the http request and do crud actions in the the browser
+  # lead somewhere in the controller 
+  # combo url + http request
+
+  # root 'controller#action' - home page to the url pattern
+  # root 'static#home'
+  # resources :controller - gen all the routes and actions for a controller
+  # resources :contacts
+  # - with relationships with models you are going to embed two levels deep
+  # resources :contacts do 
+  #   resources :notes
+  # end
+
+  # bad 
+  # resources :contacts do 
+  #   resources :notes do 
+  #     resources :comments 
+  #   end
+  # end
+
+  # best practice 
+  # resources :contacts do 
+  #   resources :notes
+  # end
+
+  # resources :notes  do
+  #   resources :comments
+  # end
+  # custom routes - doing the routes  by hand 
+  # httpverb 'pattern', to: 'controller#action'
+  # get        Read
+  # post        create
+  # put / patch  update
+  # delete       destroy
+  # get '/contacts', to: 'contacts#index'
+  # /billboards/:id
+  # /billboards/:parent_id
+  # pass in a id for the route to work
+  # billboards/2
+  root 'billboards#index'
+
+  resources :billboards do 
+    resources :artists
+  end
+
+  # bad 
+  # resources :billboards do 
+  #   resources :artists do
+  #     resources :comments
+  #   end
+  # end
+  # resources :artists, only: [:index, :new] do
+  #   resources :comments
+  # end
+  resources :artists, except: [:index, :new, :create, :edit, :show, :update, :destroy] do
+    resources :comments
+  end
 end
